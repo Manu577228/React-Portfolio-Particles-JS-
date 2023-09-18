35ae7fe1205851edf6ce96117ff09c94cb7a9d7e
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2023-09-18 15:43:41 +0530

    First Commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,48 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Employee Management System",
+        tags:["React","Java-SpringBoot","My-SQL"],
+        imgSrc:"https://empmonitor.com/blog/wp-content/uploads/2020/04/Importance-Of-Employee-Management-System.png",
+        link:"https://github.com/Manu577228/Employee-Management-System/tree/master"
+    },
+    {
+        id:2,
+        name:"Google Search Engine Clone",
+        tags:["React","React-Icons"],
+        imgSrc:"https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Google_Homepage.PNG/1200px-Google_Homepage.PNG",
+        link:"https://github.com/Manu577228/Next.JS-Google-Search-Clone"
+    },
+    {
+        id:3,
+        name:"Data Structure & Algorithms( Problem solving in Javascript with explaination )",
+        tags:["Javascript"],
+        imgSrc:"https://leetcode.com/static/images/LeetCode_Sharing.png",
+        link:"https://github.com/Manu577228/DSA-Favourite-Fifty-Javascript"
+    },
+    {
+        id:4,
+        name:"E-commerce Website",
+        tags:["React","Redux-Toolkit"],
+        imgSrc:"https://colorlib.com/wp/wp-content/uploads/sites/2/ecommerce-website-builder.jpg",
+        link:"https://github.com/Manu577228/Redux-Tutorial-Project"
+    },
+    {
+        id:5,
+        name:"Full-Stack Login Register App",
+        tags:["MongoDB","Express","React","Node"],
+        imgSrc:"https://i.ytimg.com/vi/v95sxoEUQpI/maxresdefault.jpg",
+        link:"https://github.com/Manu577228/MERN-Login-Register-App"
+    },
+    {
+        id:6,
+        name:"Shopping-Cart App",
+        tags:["React","Typescript","Bootstrap"],
+        imgSrc:"https://www-cdn.bigcommerce.com/assets/answers/shopping-carts-are-an-important-aspect.jpg",
+        link:"https://github.com/Manu577228/React-ShoppingCart-App"
+
+
+    },
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"My Recent Portfolio",
+        description:"A portfolio exemplifies your beliefs, skills, qualifications, education, training, and experiences.",
+        tags:["React","Bootstrap","Email-JS"],
+        
+        demo:"https://manu-bharadwaj-portfolio.vercel.app/",
+        github:"https://github.com/Manu577228/My-New-Portfolio"
+    },
+    {
+        id:2,
+        name:"My Recent Portfolio - 2",
+        description:"A portfolio exemplifies your beliefs, skills, qualifications, education, training, and experiences.",
+        tags:["Next-JS","React","TailwindCss"],
+        
+        demo:"https://manu-portfolio-woad.vercel.app/",
+        github:"https://github.com/Manu577228/My-Portfolio"
+        
+    },
+    {
+        id:3,
+        name:"Bharadwaj E-Shop",
+        description:"An ecommerce website is your digital storefront on the internet. It facilitates the transaction between a buyer and seller.",
+        tags:["Next-JS","React","sanity","Stripe"],
+        
+        demo:"https://bharadwaj-e-shop.vercel.app/",
+        github:"https://github.com/Manu577228/FullStack-ECommerce-App"
+    },
+    {
+        id:4,
+        name:"My Blog",
+        description:"The purpose of a blog is to create a more engaging website for your brand.",
+        tags:["Next-JS","React","TailwindCss"],
+        
+        demo:"https://manu-blog.vercel.app/",
+        github:"https://github.com/Manu577228/Blog-Using-Next.JS"
+       
+    },{
+        id:5,
+        name:"My old Portfolio",
+        description:"A portfolio exemplifies your beliefs, skills, qualifications, education, training, and experiences.",
+        tags:["React","Three-JS","Firebase"],
+        
+        demo:"https://manu-portfolio-3d.web.app/",
+        github:"https://github.com/Manu577228/3D-Portfolio"
+        
+    },
+    {
+        id:6,
+        name:"Bharadwaj E-Learning",
+        description:"eLearning, or electronic learning, is the delivery of learning and training through digital resources.",
+        tags:["Angular","Material-Design","Bootstrap"],
+        
+        demo:"https://bharadwaj-e-learning.vercel.app",
+        github:"https://github.com/Manu577228/Angular-E-Learning-website"
+        
+    },
+    
+  
+] 
